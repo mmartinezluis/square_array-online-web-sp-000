47d92a6ce69aaab8a549415cb4b681edba47fae2
Luis Martinez <luis_mmartinez@live.com>
2020-12-27 04:35:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,9 @@
 require "pry"
 def square_array(array)
-  # your code here
-  binding.pry
   new_array=[ ]
   array.each do |item|
     item=(item)**2
     new_array.push(item)
   end
-  new_array
+  return new_array
 end
